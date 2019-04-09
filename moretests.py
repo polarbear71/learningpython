ea4cfc848bf5a653bcf6c64090de30bad7cdbef6
Polarbear72 <polarbear72@web.de>
2019-04-09 20:19:42 +0200

    mutable and immutable types ... and more

---
+++ moretests.py
@@ -0,0 +1,48 @@
+#! usr/bin/env python
+
+namensliste = []
+
+''' with open("Namen.txt", "r") as freadobj:
+    for line in freadobj:
+        print(line.strip('\n'))
+        namensliste.append(line.strip())
+
+with open("NamenErgebnis.txt", "w") as fwriteobj:
+    for line in namensliste:
+        fwriteobj.write(line) '''
+
+''' for i in range(0, 10):
+    namensliste.append("zahl " + str(i)) '''
+
+''' zahl1 = 123
+zahl2 = zahl1
+zahl2 = 234
+
+print(type(zahl1))
+print(id(zahl1))
+
+zahl3 = 3333 '''
+
+a = 1
+b = 1
+print(id(a))
+print(id(b))
+a += 3
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+
+list1 = [1,2]
+print(id(list1))
+list2 = list1
+print(id(list2))
+list1.append(3)
+
+a = 5
+
+
+
+
+
+
