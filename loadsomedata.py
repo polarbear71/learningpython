39fd24b4e9620cf520cdbb0a3b18c929f9375baf
polarbear71 <45788246+polarbear71@users.noreply.github.com>
2019-11-27 12:55:14 +0100

    commented out as keras not found

--- loadsomedata.py
+++ loadsomedata.py
@@ -1,4 +1,9 @@
 #! /usr/bin/env python
 
-  
 print("Schreibe irgend einen Text in die Konsole")
+
+'''from keras.datasets import mnist
+train_da, test_da = mnist.load_data()
+x_train, y_train = train_da
+x_test, y_test = test_da'''
+
